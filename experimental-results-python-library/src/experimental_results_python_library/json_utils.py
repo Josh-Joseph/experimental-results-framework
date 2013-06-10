def6b2232c1da6a958ec3d979e9b776d21674989
velezj <velezj@alum.mit.edu>
2013-06-10 14:48:17 -0400

    Added a decent implementation of podify. Also added ability to register trasnformations for podify for when we need a better system rather than a fixed ordering :-)

--- experimental-results-python-library/src/experimental_results_python_library/json_utils.py
+++ experimental-results-python-library/src/experimental_results_python_library/json_utils.py
@@ -1,12 +1,153 @@
 
 import json
 
+#----------------------------------------------------------------------------
 
+def is_hashable( obj ):
+    """Returns true iff the given object is hashable.
+       This computes the hash itself, so it works every time :-)"""
+    
+    try:
+        hash( obj )
+    except TypeError:
+        return False
+    return True
+
+
+#----------------------------------------------------------------------------
+
+##
+## Description:
+## A global list of registered podify transformation.
+## This list is ordered, and each element is a tuple with
+## ( predicate,  transfrom, name ) where the first two are functions.
+##
+## predicte: python object -> True if tranform should be applied
+## transform: python object -> POD object
+## name: a string
+_registered_podify_transforms = []
+
+
+#----------------------------------------------------------------------------
+
+def register_podify_transform( predicate, transform, name="" ):
+    """Register a podify transform with an optional name.
+       Returns a trasnform token useful to remove the transform later on
+       if wanted.
+       
+       Arguments:
+       predicate -- A one-argument function which returns true iff the given 
+                    tranform function should be applied to podify an object.
+       transform -- A one-argument function which *must* return A POD version
+                    of any object which passes the predicate.
+       name (optiona) -- A name for this transform (mostly for debugging)
+       """
+    
+    _registered_podify_transforms.append( (predicate, transform, name ) )
+    return len( _registered_podify_transforms ) - 1
+
+
+#----------------------------------------------------------------------------
+
+def unregister_podify_transform( transform_token ):
+    """Removes the given trasnform token (as returned by 
+       register_podify_transform) form the transforms used by podify."""
+    
+    del _registered_podify_transforms[ transform_token ]
+
+
+#----------------------------------------------------------------------------
+
+def _try_podify_transforms( obj ):
+    """Iterates over all registered podify transforms and uses the first one
+       whoose predicate returns true for the given object.
+       Returns two values: ( transform-result, found )
+       where found is True if a transform was run, and the resutls of the
+       transform are returned in the first value.
+       The second result is used to determine whetehr a transform was run and
+       returned None."""
+    
+    for pred,trans,name in _registered_podify_transforms:
+        if pred( obj ):
+            return trans( obj ), True
+    return None, False
+
+#----------------------------------------------------------------------------
+
+def podify( obj, use_transforms=True ):
+    """Returns a "Plain-0l-Datatype" version of the given object.
+       This means that the returned object comporises only of the
+       following:
+         dict
+         list
+         int
+         float
+         string
+       This method recusively calls podify in a structured object to make
+       sure the above types are the only ones used. Mainly it calls str()
+       on any object which is not the above type :-).
+       
+       Also of note, one can register a "podify transform" with the module.
+       Transforms include a predicate determinig whether it should be applied,
+       and are assumed to return a valid POD type once called with an object.
+       You can of course recourse podify(...) inside a transform, but make sure
+       you do not cause infinite loops (or call with use_transforms=False )"""
+
+    # First, check if we are using transforms and there are any that match
+    if use_transforms and _registered_podify_transforms:
+        pod_obj, found = _try_podify_transforms( obj )
+        if found:
+            return pod_obj
+    
+    # Either no trasnforms registerd for this object, or we are not using them
+    # Try a fixed ordering for podify types
+    if isinstance( obj, str ):
+        return obj
+    if isinstance( obj, int ):
+        return obj
+    if isinstance( obj, float ):
+        return obj
+    if isinstance( obj, list ):
+        return map( podify, obj )
+
+    # dictionaries are special, we first check each item to see if a transform
+    # is registered, if not then we podify key and value separately
+    if isinstance( obj, dict ):
+        pod_t = {}
+        for k,o in obj.iteritems():
+            found = False
+            if use_transforms and _registered_podify_transforms:
+                (tk,to), found = _try_podify_transforms( (k,o) )
+            if not found:
+                tk = podify( k )
+                if not is_hashable( tk ):
+                    tk = str(tk)
+                to = podify( o )
+            pod_t[ tk ] = to
+        return pod_t
+    if isinstance( obj, tuple ):
+        return podify( list( obj ) )
+    if isinstance( obj, type ):
+        return str( obj.__name__ )
+    return str(obj)
+
+    
+
+#----------------------------------------------------------------------------
 
 def to_json( obj ):
-    return json.dumps( obj, separators=(',',':') )
+    """Return a JSON represetation of the given python object as a string.
+       This method first calls podify( obj ) and then transform the now
+       POD object into JSON encoding."""
+    
+    return json.dumps( podify( obj ), separators=(',',':') )
 
+#----------------------------------------------------------------------------
 
 def from_json( json_str ):
+    """Load JSON encoded object from the given string into a python object.
+       The JSON string will have only POD types, hence the returned python
+       object will be a POD object as well."""
+
     return json.loads( json_str )
 
